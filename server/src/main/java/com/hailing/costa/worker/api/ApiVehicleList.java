96b7483194467f1d598f92615cdf8693a7bc128d
Hailing Zhang <hailing.zhang@ist.com>
2023-02-04 11:08:39 +0100

    Add cache and set the schedule that read the provided data every hour

--- server/src/main/java/com/hailing/costa/worker/api/ApiVehicleList.java
+++ server/src/main/java/com/hailing/costa/worker/api/ApiVehicleList.java
@@ -1,12 +1,18 @@
 package com.hailing.costa.worker.api;
 
+import java.util.List;
+
 import com.hailing.costa.entity.VehicleEntity;
 
 public class ApiVehicleList {
-  private VehicleEntity[] vehicles;
+  private List<VehicleEntity> vehicles;
   public static String url = "/list";
 
-  public VehicleEntity[] getVehicles() {
+  public ApiVehicleList(List<VehicleEntity> vehicles) {
+    this.vehicles = vehicles;
+  }
+
+  public List<VehicleEntity> getVehicles() {
     return vehicles;
   }
 }
--- server/src/main/java/com/hailing/costa/worker/api/RequestData.java
+++ server/src/main/java/com/hailing/costa/worker/api/RequestData.java
@@ -1,70 +1,73 @@
 package com.hailing.costa.worker.api;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.google.gson.Gson;
+import com.hailing.costa.dao.VehicleDao;
+import com.hailing.costa.entity.VehicleEntity;
+import com.hailing.costa.utils.HttpRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import com.hailing.costa.dao.VehicleDao;
-import com.hailing.costa.entity.VehicleEntity;
-import com.hailing.costa.utils.HttpRequest;
-
 @Async
 @Component
 @EnableScheduling
-public class RequestData extends Thread {
-  final private String BASE_URL = "http://localhost:1337/vehicle";
-  @Autowired
-  private VehicleDao vehicleDao;
+public class RequestData {
+    final private String BASE_URL = "http://localhost:1337/vehicle";
+    @Autowired
+    private VehicleDao vehicleDao;
+    private Gson gson;
+    private HttpRequest client;
 
-  private Gson gson;
-  private HttpRequest client;
+    @CacheEvict(cacheNames = {"vehicleList", "vehicleMap"})
+    @Scheduled(initialDelay = 0, fixedDelay = 60 * 60 * 10000)
+    public void run() {
+        this.gson = new Gson();
+        this.client = new HttpRequest();
 
-  @Scheduled(cron = "0/5 * * * * ?")
-  @Override
-  public void run() {
-    request();
-  }
+        // Vehicle List
+        List<VehicleEntity> vehicleList = this.getVehicleList();
 
-  private void request() {
-    this.gson = new Gson();
-    this.client = new HttpRequest();
-
-    // Vehicle List
-    VehicleEntity[] vehicleList = this.getVehicleList();
-
-    // Vehicle Info
-    for (int i = 0; i < vehicleList.length; i++) {
-      this.getVehicleInfo(vehicleList[i]);
+        // Vehicle Info
+        for (VehicleEntity vehicle : vehicleList) {
+            this.getVehicleInfo(vehicle);
+        }
     }
-  }
 
-  private VehicleEntity[] getVehicleList() {
-    String str = this.client.doGet(BASE_URL + ApiVehicleList.url);
-    ApiVehicleList json = this.gson.fromJson(str, ApiVehicleList.class);
-    if (json == null) {
-      System.out.println("Get Vehicle List Failed!");
-      return new VehicleEntity[0];
+    private List<VehicleEntity> getVehicleList() {
+        Map<String, VehicleEntity> map = this.vehicleDao.getMap();
+        System.out.println(map.keySet());
+        String str = this.client.doGet(BASE_URL + ApiVehicleList.url);
+        ApiVehicleList json = this.gson.fromJson(str, ApiVehicleList.class);
+        if (json == null) {
+            System.out.println("Get Vehicle List Failed!");
+            return new ArrayList<>();
+        }
+        List<VehicleEntity> list = json.getVehicles();
+        list.stream().filter(vehicleEntity -> map.get(vehicleEntity.getId()) == null).collect(Collectors.toList());
+        if (!list.isEmpty()) {
+            list.forEach(vehicleEntity -> this.vehicleDao.insert(vehicleEntity));
+            System.out.println("Update Vehicle List Success (" + list.size() + ")");
+        }
+        return list;
     }
-    VehicleEntity[] list = json.getVehicles();
-    for (int i = 0; i < list.length; i++) {
-      this.vehicleDao.insert(list[i]);
-    }
-    System.out.println("Get Vehicle List Success!");
-    return list;
-  }
 
-  private void getVehicleInfo(VehicleEntity item) {
-    String identy = item.getId() + ": " + item.getName();
-    String str = client.doGet(BASE_URL + ApiVehicleInfo.url + "?id=" + item.getId());
-    ApiVehicleInfo json = this.gson.fromJson(str, ApiVehicleInfo.class);
-    if (json == null) {
-      System.out.println("Get Vehicle Info Failed - " + identy);
-      return;
+    private void getVehicleInfo(VehicleEntity item) {
+        String identity = item.getId() + ": " + item.getName();
+        String str = client.doGet(BASE_URL + ApiVehicleInfo.url + "?id=" + item.getId());
+        ApiVehicleInfo json = this.gson.fromJson(str, ApiVehicleInfo.class);
+        if (json == null) {
+            System.out.println("Get Vehicle Info Failed - " + identity);
+            return;
+        }
+        this.vehicleDao.updateById(item.getId(), json.getEntity());
+        System.out.println("Update Vehicle Info Success - " + identity);
     }
-    this.vehicleDao.updateById(item.getId(), json.getEntity());
-    System.out.println("Get Vehicle Info Success - " + identy);
-  }
 }
