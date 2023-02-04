6b05e30cb0f19f064d5444aa4b0012ea987c78ef
Hailing Zhang <hailing.zhang@ist.com>
2023-02-04 09:39:31 +0100

    Build spring boot framework

---
+++ server/src/main/java/com/hailing/costa/CostaApplication.java
@@ -0,0 +1,13 @@
+package com.hailing.costa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostaApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/hailing/costa/CostaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hailing.costa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
